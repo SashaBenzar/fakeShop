5364fe66e67cb14c4dd733ad3eb2c83fef5b78ab
SashaBenzar <sahabenzar@gmail.com>
2023-04-03 17:33:24 +0300

    adding axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 interface Item {
-  "id": number,
-  "title": string,
-  "image": string,
-  "price": number,
-  "description": string,
-  "category": string,
-  "rating": { "rate": number, "count": number },
-};
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+  category: string;
+  rating: { rate: number; count: number };
+}
 
 function App() {
-
   const [items, setItems] = React.useState<Item[]>();
 
   React.useEffect(() => {
-    fetch('https://fakestoreapi.com/products/')
-      .then(res => res.json())
-      .then(json => setItems(json))
+    axios.get('https://fakestoreapi.com/products/').then(({ data }) => setItems(data));
   }, []);
 
   return (
     <div className="App">
       {items && items.map((item: Item) => <img src={item.image} key={item.id} />)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
