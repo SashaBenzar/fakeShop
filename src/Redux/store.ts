4548922d7fccf476a0c38a1debd32c75a742f75b
SashaBenzar <sahabenzar@gmail.com>
2023-04-29 20:42:24 +0300

    add local save

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,17 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import sortReducer from './Slice/sortSlice';
 import basketReducer from './Slice/basketSlice';
 import { productsApi } from './Slice/productsSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['basket'],
+};
+
+const rootReducer = combineReducers({
+  sort: sortReducer,
+  basket: basketReducer,
+  [productsApi.reducerPath]: productsApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    sort: sortReducer,
-    basket: basketReducer,
-    [productsApi.reducerPath]: productsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware),
+  reducer: persistedReducer,
+  middleware: [productsApi.middleware, thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { store } from './Redux/store';
 import { Provider } from 'react-redux';
+import { persistor, store } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
